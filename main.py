8ff24d65038f39e2038c4786d57a4e5694f21573
Sid Moparthi <siddhu.moparthi@gmail.com>
2025-12-01 00:01:28 -0500

    Implement: 12
    12

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-    title="11",
-    description="FastAPI application that returns 11",
+    title="12",
+    description="FastAPI application that returns 12",
     version="0.1.0",
 )
 
 @app.get("/")
 async def root():
-    """Return the number 11."""
-    return {"value": 11}
+    """Return the number 12."""
+    return {"value": 12}
 
 if __name__ == "__main__":
     import uvicorn
